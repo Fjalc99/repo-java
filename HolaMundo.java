9cd70613689feb8c17166322ddfb9d3bada57cbb
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2023-11-14 14:17:55 +0100

    3v.0

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo{
 
     public static void main (String [] args) {
         System.out.println("###########");
-        System.out.println("Hola Mundo");
+        System.out.println("Hello World");
         System.out.println("###########");
         adassafafdsa
     }
